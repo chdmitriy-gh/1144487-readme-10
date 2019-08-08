6e0c0f92fb6c59ae3e577f73b560b53fa047cd0a
chdmitriy-gh <ch_dmitriy@mail.ru>
2019-08-08 22:01:17 +0300

    Выводит имя (строгая проверка равенства)

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = "Дмитрий"; // укажите здесь ваше имя
+$user_name = 'Дмитрий'; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -40,7 +40,7 @@ $user_name = "Дмитрий"; // укажите здесь ваше имя
             </div>
         </form>
         <div class="header__nav-wrapper">
-            <?php if ($is_auth==1): ?>
+            <?php if ($is_auth === 1): ?>
             <nav class="header__nav">
                 <ul class="header__my-nav">
                     <li class="header__my-page header__my-page--popular">
